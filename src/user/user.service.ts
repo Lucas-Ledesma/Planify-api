4ba78a47447efa06e71873b82b350f8f1046d08a
LcuasLedesma <lucasfidel99@gmail.com>
2024-01-23 17:29:23 -0300

    user crud finish

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,26 +1,51 @@
-import { Injectable } from '@nestjs/common';
+import { ForbiddenException, Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
 export class UserService {
-  create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
+  constructor(private prisma: PrismaService) {}
+
+  async create(createUserDto: CreateUserDto) {
+    const findUser = await this.prisma.user.findUnique({
+      where: { email: createUserDto.email },
+    });
+
+    if (findUser) {
+      throw new ForbiddenException('Credentials already taken');
+    }
+
+    const user = await this.prisma.user.create({
+      data: { email: createUserDto.email, password: createUserDto.password },
+    });
+
+    return { user };
   }
 
-  findAll() {
-    return `This action returns all user`;
+  async findAll() {
+    return this.prisma.user.findMany();
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} user`;
+  async findOne(id: string) {
+    const foundUser = await this.prisma.user.findUnique({ where: { id } });
+
+    if (!foundUser) {
+      throw new ForbiddenException('Wrong Id');
+    }
+
+    return foundUser;
   }
 
-  update(id: number, updateUserDto: UpdateUserDto) {
-    return `This action updates a #${id} user`;
-  }
+  async delete(id: string) {
+    const foundUser = await this.prisma.user.findUnique({ where: { id } });
 
-  remove(id: number) {
-    return `This action removes a #${id} user`;
+    if (!foundUser) {
+      throw new ForbiddenException('Wrong Id');
+    }
+
+    await this.prisma.user.delete({ where: { id } });
+
+    return { msg: 'User was deleted sucessfully' };
   }
 }
